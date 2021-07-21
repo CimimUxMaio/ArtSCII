6513c963bceb2fc031ce3234e341f2bdb65e9dc8
CimimUxMaio <cimimuxmaio@gmail.com>
2021-07-21 17:17:13 -0300

    .ascii creation and loading

--- artscii.go
+++ artscii.go
@@ -1,24 +1,26 @@
 package artscii
 
 import (
+	"errors"
 	"fmt"
 	"image"
 	"image/color"
+	"io"
 	"math"
 	"os"
 )
 
 type AsciiImage struct {
-	scale  []byte
-	pixels [][]byte
+	scale  []rune
+	pixels [][]rune
 }
 
-func FromImage(img image.Image, scale []byte) *AsciiImage {
+func FromImage(img image.Image, scale []rune) *AsciiImage {
 	bounds := img.Bounds()
 	width, height := bounds.Max.X, bounds.Max.Y
-	var pixels [][]byte
+	var pixels [][]rune
 	for y := 0; y < height; y++ {
-		var row []byte
+		var row []rune
 		for x := 0; x < width; x++ {
 			gray := color.GrayModel.Convert(img.At(x, y))
 			r, _, _, _ := gray.RGBA()
@@ -31,7 +33,7 @@ func FromImage(img image.Image, scale []byte) *AsciiImage {
 	return &AsciiImage{scale: scale, pixels: pixels}
 }
 
-func FromFile(filePath string, scale []byte) (*AsciiImage, error) {
+func FromImageFile(filePath string, scale []rune) (*AsciiImage, error) {
 	file, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
@@ -44,6 +46,39 @@ func FromFile(filePath string, scale []byte) (*AsciiImage, error) {
 	}
 
 	return FromImage(image, scale), nil
+}
+
+var ErrBadFormat = errors.New("the given file is not of type .ascii or its corrupted")
+
+func FromArtSCIIFile(filePath string) (*AsciiImage, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	buff, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	i, content := 0, string(buff)
+	var rows [][]rune
+	for _, c := range content {
+		if c == '\n' {
+			i += 1
+			continue
+		}
+
+		rows[i] = append(rows[i], c)
+	}
+
+	if len(rows) < 1 {
+		return nil, ErrBadFormat
+	}
+
+	scale := rows[0]
+	return &AsciiImage{scale: scale, pixels: rows[1:]}, nil
 }
 
 func (ascii *AsciiImage) Dim() (width, height int) {
@@ -60,4 +95,24 @@ func (ascii *AsciiImage) Print() {
 	for _, row := range ascii.pixels {
 		fmt.Println(string(row))
 	}
+}
+
+func (ascii *AsciiImage) ToFile(path string) (*os.File, error) {
+	filePath := path + ".artscii"
+
+	file, err := os.Create(filePath)
+	if err != nil {
+		return nil, err
+	}
+
+	var content []byte = []byte(string(ascii.scale))
+	for _, row := range ascii.pixels {
+		content = append(content, []byte(string(row)+"\n")...)
+	}
+	_, err = file.Write(content)
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
 }
